169fa59f67f77b083c6e15d0f5b9dc333a9ffcdd
gomezjonathan46 <gomez.e.jonathan@gmail.com>
2026-04-28 06:19:03 -0400

    Move ImageTextSection below hero image

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 import ImageTextSection from "../src/components/ImageTextSection";
-import Navbar from "../src/components/nav"
-import Hero from '../src/components/Hero';
+import Navbar from "../src/components/nav";
+import Hero from "../src/components/Hero";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
+      <Hero />
       <ImageTextSection
         title="Our Story"
         description="Hot Stuff Dining brings bold flavors and late-night comfort food."
         imageSrc="/../src/assets/chef.jpg"
       />
-      <Navbar/>
-      <Hero />
     </div>
   );
 }
